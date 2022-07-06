d530816b967514bc9c8dff88b5d7e0278929fa91
Khola94 <ahmed.khola94@gmail.com>
2022-07-06 13:32:10 +0100

    Weekly forecast api integrated

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let h1 = document.querySelector("h1");
-h1.innerHTML = new Date().toUTCString().slice(0, 16);
-
-document.querySelector(".time").innerHTML = new Date()
-  .toLocaleString()
-  .slice(11, 17);
-
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -23,6 +16,15 @@ function displayWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = new Date().toUTCString().slice(0, 16);
+
+  document.querySelector(".time").innerHTML = new Date()
+    .toLocaleString()
+    .slice(11, 17);
 }
 
 function searchCity(city) {
@@ -79,3 +81,51 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", dispalyCelsiusTemperature);
 
 searchCity("London");
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="80"
+        />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </div>
+          <div class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </div>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
